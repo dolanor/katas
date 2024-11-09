a7c7f486dbf7754c14bf4585007baec5cd227024
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2024-11-09 15:38:59 +0100

    test rule 1

--- gol/3/main_test.go
+++ gol/3/main_test.go
@@ -21,3 +21,23 @@ exp:
 %s`, got, exp)
 	}
 }
+
+func TestGridRule1(t *testing.T) {
+	exp := `
+11···
+·····
+·····
+·····
+·····`
+	grid := NewGrid()
+	grid.Init(true, true)
+
+	got := grid
+	if got != exp {
+		t.Fatalf(`got:
+%s
+
+exp:
+%s`, got, exp)
+	}
+}
